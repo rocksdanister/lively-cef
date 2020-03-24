366a08224497267f2fde4740aec25cd09429f2bc
Dani John <rocksdanister@gmail.com>
2020-03-24 07:45:13 +0530

    v4.1
    Preparing for lively mouse input support:
    - Right click contextmenu disable.
    - Opening new links in the same cef window.
    - Enabled mouse events in shadertoy link parser.

---
+++ src/cefsharptest/Cef/CefPopUpHandle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CefSharp;
+
+namespace cefsharptest
+{
+    class CefPopUpHandle : ILifeSpanHandler
+    {
+        public bool DoClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            return false;
+        }
+
+        public void OnAfterCreated(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            
+        }
+
+        public void OnBeforeClose(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+            
+        }
+
+        public bool OnBeforePopup(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, string targetFrameName, WindowOpenDisposition targetDisposition, bool userGesture, IPopupFeatures popupFeatures, IWindowInfo windowInfo, IBrowserSettings browserSettings, ref bool noJavascriptAccess, out IWebBrowser newBrowser)
+        {
+            browser.MainFrame.LoadUrl(targetUrl);
+            newBrowser = null;
+            return true;
+        }
+    }
+}
